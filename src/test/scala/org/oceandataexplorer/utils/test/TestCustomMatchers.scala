041990d3246120e017fa8bce1ac30ee860cfd036
Alex2242 <alex@degurse.fr>
2018-07-12 09:32:57 +0200

    Add Custom Matchers

---
+++ src/test/scala/org/oceandataexplorer/utils/test/TestCustomMatchers.scala
@@ -0,0 +1,39 @@
+/** Copyright (C) 2017-2018 Project-ODE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.oceandataexplorer.utils.test
+
+import org.scalatest.{FlatSpec, Matchers}
+
+
+/**
+ * Tests for CustomMatchers
+ *
+ * @author Alexandre Degurse
+ */
+class TestCustomMatchers extends FlatSpec with Matchers with CustomMatchers {
+
+  "ErrorMetrics" should "compute the rmse for two sequences" in {
+    val seqA = Array(
+      0.1221174368357885, 0.8030612655311997, 0.8732426284336273,
+      0.8000925604778708, 0.6351656368136573, 0.323284190497698 ,
+      0.6192489942098376, 0.9573403084388671, 0.7101131243855894,
+      0.0232360227774637
+    )
+
+    seqA should rmseMatch(seqA)
+  }
+}
